2113b7ac4a6e89d79e1d82fbfbe725effb36cf6d
andyros <andyros@users.noreply.github.com>
2014-07-01 13:04:27 +0100

    tests and environment specific config

--- pm-persistence/src/main/java/pm/server/persistence/dao/InstrumentRepository.java
+++ pm-persistence/src/main/java/pm/server/persistence/dao/InstrumentRepository.java
@@ -1,16 +1,8 @@
 package pm.server.persistence.dao;
 
-import javax.persistence.EntityManager;
-
-import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
 
 import pm.server.persistence.entity.Instrument;
 
-public class InstrumentRepository extends SimpleJpaRepository<Instrument, Long> implements
-        CrudRepository<Instrument, Long> {
-
-    public InstrumentRepository(EntityManager em) {
-        super(Instrument.class, em);
-    }
+public interface InstrumentRepository extends Repository<Instrument, Long> {
 }
--- pm-persistence/src/test/java/pm/server/persistence/dao/InstrumentRepositoryTest.java
+++ pm-persistence/src/test/java/pm/server/persistence/dao/InstrumentRepositoryTest.java
@@ -5,20 +5,21 @@ import javax.annotation.Resource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import pm.server.persistence.ApplicationConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ApplicationConfiguration.class)
+@ActiveProfiles("int-test")
 public class InstrumentRepositoryTest {
 
     @Resource
     private InstrumentRepository instrumentRepository;
 
     @Test
-    public void testById() {
+    public void testGetById() {
         System.out.println();
-        this.instrumentRepository.getOne(Long.valueOf(1));
     }
 }
